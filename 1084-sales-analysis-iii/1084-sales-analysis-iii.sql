9f98ac16b149abdb02bc2d46b0105af74b0fa46a
Harsh <harshrew@gmail.com>
2026-08-26 18:13:40 +0530

    Time: 1402 ms (28.02%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+select p.product_id,p.product_name
+from Product p
+left join Sales s
+on p.product_id=s.product_id
+group by p.product_id,product_name
+having min(sale_date)>='2019-01-01'
+and max(sale_date)<='2019-03-31';
