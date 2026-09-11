fa06b53639d86cf0f4222b5108f81dbb35f34cde
Harsh <harshrew@gmail.com>
2026-09-11 15:18:51 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.2%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+    int n = s.size();
+    stack<char>st;
+    for(int i=0;i<n;i++){
+
+    if(s[i]=='('||s[i]=='['||s[i]=='{') st.push(s[i]);
+
+    else if(s[i]==')'){
+        if(st.empty()) return false;
+        if(st.top() == '[' || st.top() == '{') return false;
+            st.pop();
+    }
+    else if(s[i]=='}'){
+        if(st.empty()) {
+                return false;
+            }
+        if(st.top()=='['||st.top()=='(') return false;
+        
+            st.pop();
+    }
+    else{
+        if(st.empty()) {
+                return false;
+            }
+        if(st.top()=='{'||st.top()=='(') return false;
+        
+            st.pop();
+
+    }
+    
+    }    
+    if(!st.empty()) return false;
+    return true;
+    
+    }
+};
