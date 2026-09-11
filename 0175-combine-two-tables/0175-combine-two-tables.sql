64726d8b357abd987bef4b957cd52eb5432cd1b4
Harsh <harshrew@gmail.com>
2026-09-11 19:09:33 +0530

    Time: 580 ms (13.15%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,3 @@
-select p.firstName,P.lastName,a.city,a.state
-from Person p
-left join Address a
+select firstName,lastName,city ,state from Person p
+left join Address a 
 on p.personId=a.personId;
