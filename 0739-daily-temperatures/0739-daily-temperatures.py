72f76880c258e61d37a125d8b5ecc90273a35ec4
Harsh <harshrew@gmail.com>
2026-09-15 21:01:18 +0530

    Time: 212 ms (12.83%), Space: 27.3 MB (12.35%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        n=len(temperatures)
+        st=[]
+        ans=[]
+        for i in range (n-1,-1,-1):
+            while st and temperatures[st[-1]]<=temperatures[i] :   
+                st.pop()
+            if not st :
+                ans.append(0)
+            else :
+                ans.append(st[-1]-i)
+            st.append(i)
+
+        ans.reverse()
+        return ans
