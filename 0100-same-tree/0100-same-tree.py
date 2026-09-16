a05efff2d9fbed09a74f5976b7d69b255518125b
Harsh <harshrew@gmail.com>
2026-09-16 18:05:19 +0530

    Time: 0 ms (100%), Space: 12.4 MB (93.51%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p is None or q is None:
+            return p==q
+
+        return (p.val==q.val
+            and self.isSameTree(p.left,q.left)
+            and self.isSameTree(p.right,q.right))
+        
+
+        
