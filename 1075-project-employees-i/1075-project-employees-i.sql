6887e6411de0d3005fae56384ff5a2963b535f7b
Harsh <harshrew@gmail.com>
2026-09-16 19:07:50 +0530

    Time: 525 ms (80.03%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,6 +1,4 @@
-select project_id,
-round(avg(e.experience_years),2) as average_years
-from Project p
-left join Employee e
+select project_id,round(sum(experience_years)/count(experience_years),2) as average_years from Project p
+join Employee e
 on p.employee_id=e.employee_id
 group by project_id;
