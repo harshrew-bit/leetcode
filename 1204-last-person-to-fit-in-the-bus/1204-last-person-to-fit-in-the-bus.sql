7669fbd74c0e6c388fb058562307e426962d319f
Harsh <harshrew@gmail.com>
2026-09-20 14:25:38 +0530

    Time: 863 ms (71.34%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+select person_name 
+from(
+    select person_name,turn,
+    sum(weight) over(order by turn) as total_weight
+    from Queue
+) q
+where total_weight<=1000
+order by turn desc
+limit 1;
